7b98f7b86f664b14c09ad62faec910ee2ae61550
Belal Said <beloskhalil@gmail.com>
2015-09-05 15:41:40 -0400

    file added

---
+++ pinger.ino
@@ -0,0 +1,49 @@
+int trig = { 2, 3, 4 };
+int echo = { 5, 6, 7 };
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(vcc, HIGH);
+
+  pinMode(trig, OUTPUT);
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trig, LOW);
+
+  pinMode(echo, INPUT);
+  duration = pulseIn(echo, HIGH);
+
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  delay(100);
+}
+
+long microsecondsToInches(long microseconds)
+{
+// According to Parallax's datasheet for the PING))), there are
+// 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+// second). This gives the distance travelled by the ping, outbound
+// and return, so we divide by 2 to get the distance of the obstacle.
+// See: http://www.parallax.com/dl/docs/prod/acc/28015-PI...
+return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+// The speed of sound is 340 m/s or 29 microseconds per centimeter.
+// The ping travels out and back, so to find the distance of the
+// object we take half of the distance travelled.
+return microseconds / 29 / 2;
+}
